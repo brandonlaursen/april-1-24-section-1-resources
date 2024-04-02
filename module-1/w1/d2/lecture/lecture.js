d90748641cc998c31a3a672f42aff4ebb86f1598
brandon <brandonlaursen95@yahoo.com>
2024-04-02 11:45:48 -0400

    added comments in lecture.js

---
+++ module-1/w1/d2/lecture/lecture.js
@@ -0,0 +1,10 @@
+console.log('hello');
+
+/*
+Multi line comment
+
+git add . - add all your changes to git
+git commit -m 'message' - commit the changes
+git push - push the changes up to github
+
+*/
