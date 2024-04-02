1fcc720b162baf943d3c6f85dae82e456338f7d2
brandon <brandonlaursen95@yahoo.com>
2024-04-02 18:43:50 -0400

    loops

--- module-1/w1/d2/guided-practice/aa01-conditionals/problems/03-three-or-seven.js
+++ module-1/w1/d2/guided-practice/aa01-conditionals/problems/03-three-or-seven.js
@@ -4,30 +4,53 @@
 return the original number.
 */
 
-
 function threeOrSeven(num) {
+  // if(num === 0) return num;
 
-  if(num === 0) return num;
-  
-  if(num % 3 === 0 && num % 7 !== 0) {
-    return 3;
-  } else  if(num % 7 === 0 && num % 3 !== 0) {
-    return 7
-  } else if(num % 3 === 0 && num % 7 === 0) {
+  // if(num % 3 === 0 && num % 7 !== 0) {
+  //   return 3;
+  // } else  if(num % 7 === 0 && num % 3 !== 0) {
+  //   return 7
+  // } else if(num % 21 === 3 || num % 21 === 7) {
+  //   return 21;
+  // } else {
+  //   return num;
+  // };
+
+  if (num === 0) {
+    return num;
+  } else if (num % 21 === 0) {
     return 21;
+  } else if (num % 3 === 0) {
+    return 3;
+  } else if (num % 7 === 0) {
+    return 7;
   } else {
     return num;
+  };
+
+  // let password = 12345;
+  let password = '234fqawdf342qw3r4es';
+
+  let hashedPassword = 12345
+
+  if(hashedPassword === 12345){
+    console.log('give access')
+  } else {
+    console.log('deny access')
   }
+
 }
 
-console.log(threeOrSeven(3));   // => 3
-console.log(threeOrSeven(15));  // => 3
-console.log(threeOrSeven(7));   // => 7
-console.log(threeOrSeven(14));  // => 7
-console.log(threeOrSeven(21));  // => 21
+console.log("==>", 3 % 21);
+console.log(threeOrSeven(3)); // => 3
+console.log(threeOrSeven(15)); // => 3
+console.log(threeOrSeven(7)); // => 7
+console.log(threeOrSeven(14)); // => 7
+console.log(threeOrSeven(21)); // => 21
 console.log(threeOrSeven(-42)); // => 21
 console.log(threeOrSeven(100)); // => 100
-console.log(threeOrSeven(0));   // => 0
+console.log(threeOrSeven(0)); // => 0
 console.log(threeOrSeven(-20)); // => -20
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
---
+++ module-1/w1/d2/lecture/loops.js
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+for(let i = 0; i < 5; i++ ) {
+  // console.log(i);
+}
+
+
+// for(let i = string.length - 1; i >= 0; i--)
+// let i = string.length - 1
+
+
+// while(i >= 0) {
+//   console.log(i);
+
+//  i--
+// }
+
+
+
+for(let i = 5; i <= 100; i+= 10){
+  // console.log(i);
+}
+
+
+string = 'brandon';
+/*
+  b r a n d o n
+  0 1 2 3 4 5 6
+*/
+
+for(let i = 0; i < string.length; i++ ){
+
+  // let character = string[i];
+  // console.log(i, character);
+}
+
+//  console.log(string[0])
+//  console.log(string[1])
+//  console.log(string[2])
+//  console.log(string[3])
+//  console.log(string[4])
+//  console.log(string[5])
+//  console.log(string[6])
+
+string = 'brandon';
+/*
+  b r a n d o n
+  0 1 2 3 4 5 6
+*/
+
+// console.log(string.length - 1)
+for(let i = 6; i >= 0; i--){
+  console.log(i)
+  // console.log(string[i]);
+}
+
+let start = 0
+let end = 10
+for(let i = start; i < end; i++){
+  // console.log(i);
+}
+
+
+let obj = {
+  val: 1,
+  next: { val: 2, next: { val: 3, next:null}}
+}
+
+// while(obj.next){
+
+// }
