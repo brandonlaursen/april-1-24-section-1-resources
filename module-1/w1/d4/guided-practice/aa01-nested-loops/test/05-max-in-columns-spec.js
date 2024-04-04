ea356250a92c6c74634ac97621b91fab7668f6bd
brandon <brandonlaursen95@yahoo.com>
2024-04-04 12:55:51 -0400

    nested loops

---
+++ module-1/w1/d4/guided-practice/aa01-nested-loops/test/05-max-in-columns-spec.js
@@ -0,0 +1,18 @@
+const chai = require("chai");
+const expect = chai.expect;
+const maxColumn = require("../problems/05-max-in-columns");
+
+describe("maxColumn", function () {
+  it("maxColumn(matrix) should return a an array containing the maximum value in each column", function () {
+    expect(maxColumn(matrix)).to.be.an("array");
+  });
+  it("maxColumn(matrix) should return [12, 19, 21]", function () {
+    expect(
+      maxColumn([
+        [5, 9, 21],
+        [9, 19, 6],
+        [12, 14, 15],
+      ])
+    ).to.eql([12, 19, 21]);
+  });
+});
