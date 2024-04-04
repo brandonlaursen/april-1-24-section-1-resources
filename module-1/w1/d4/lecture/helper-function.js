e102b7ba7a41338f2b4f340aeb743aac833db39b
brandon <brandonlaursen95@yahoo.com>
2024-04-04 15:48:21 -0400

    helper functions

--- module-1/w1/d4/lecture/helper-function.js
+++ module-1/w1/d4/lecture/helper-function.js
@@ -36,10 +36,7 @@ let deposit = function (initialBalance, amount) {
   return initialBalance;
 };
 
-
-
-
-function addition(num1, num2){
+function addition(num1, num2) {
   return num1 + num2;
 }
 
@@ -48,17 +45,56 @@ function addition(num1, num2){
 // let res = addition(12, 50);
 // console.log(res);// 62
 
-
 function calculator(num1, num2, operation) {
   if (operation === "add") {
     let sum = addition(num1, num2);
-    console.log(sum);// 22
-    return sum
+    console.log(sum); // 22
+    return sum;
   } else if (operation === "subtract") {
     return num1 - num2;
   }
 }
 
-console.log(calculator(10, 12, "add"));// 22
+// console.log(calculator(10, 12, "add"));// 22
 
 // console.log(calculator(10, 12, "subtract"));
+
+function myIncludes(words, target) {
+  console.log("====>", target);
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+
+    if (word === target) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// console.log(myIncludes(['an', 'example', 'sweet'], 'sweet'))
+// console.log(myIncludes(['an', 'example', 'sweet'], 'food'))
+
+function sentenceChecker(sentence) {
+  let result = [];
+  let words = sentence.split(" ");
+  // console.log(words);
+
+  let bool = myIncludes(words, "ramen");
+  
+  if (bool) {
+    result.push(bool);
+  }
+
+  // let bool2 = words.includes('ramen')
+  // if(bool2) {
+  //   result.push(bool2)
+  // }
+
+  return result;
+}
+
+console.log(
+  "sentenceChecker",
+  sentenceChecker("dont eat the ramen flavored everything bagel")
+); // [true]
