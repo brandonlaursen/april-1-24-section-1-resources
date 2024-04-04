9e16c3eec5ad3c8335235d1519101e125e6220e8
brandon <brandonlaursen95@yahoo.com>
2024-04-04 13:31:12 -0400

    pair maker

--- module-1/w1/d4/guided-practice/aa01-nested-loops/problems/01-pairs-maker.js
+++ module-1/w1/d4/guided-practice/aa01-nested-loops/problems/01-pairs-maker.js
@@ -4,15 +4,51 @@ function should return a 2D array where the subarrays represent unique pairs of
 element from the input array.
 */
 
-// Your code here 
+/*
 
-// console.log(pairsMaker(["a", "b", "c", "d"]));
-// //=> [ [ 'a', 'b' ],
-// //     [ 'a', 'c' ],
-// //     [ 'a', 'd' ],
-// //     [ 'b', 'c' ],
-// //     [ 'b', 'd' ],
-// //     [ 'c', 'd' ] ]
+
+  Understand the problem
+    input: ["a", "b", "c", "d"]
+    output: a matrix
+
+  make a plan
+  1. create a new array
+  2. iterate through the array
+    a. grab the first pair
+    b. iterate through the array start at i + 1
+      a. grab the second pair
+      c. pair the pairs up in an array and push to newArr
+  3. return new arr
+
+  execute the plan
+  refactor
+*/
+
+
+function pairsMaker(arr) {
+  let result = [];
+
+  for(let i = 0; i < arr.length; i++){
+    let pair1 = arr[i];
+    // console.log({pair1});
+
+    for(let j = i + 1; j < arr.length; j++){
+      let pair2 = arr[j];
+      // console.log({pair2});
+      result.push([pair1, pair2]);
+      // console.log(result)
+    }
+  }
+  return result;
+}
+
+console.log(pairsMaker(["a", "b", "c", "d"]));
+//=> [ [ 'a', 'b' ],
+//     [ 'a', 'c' ],
+//     [ 'a', 'd' ],
+//     [ 'b', 'c' ],
+//     [ 'b', 'd' ],
+//     [ 'c', 'd' ] ]
 
 // console.log(pairsMaker(["Rosemary", "Alex", "Connor"]));
 // //=> [ [ 'Rosemary', 'Alex' ],
