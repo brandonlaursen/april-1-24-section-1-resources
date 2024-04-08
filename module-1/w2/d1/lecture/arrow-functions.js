7860a3c2ff04be82d89f430a865743e095aabf22
brandon <brandonlaursen95@yahoo.com>
2024-04-08 14:15:37 -0400

    lunch time

---
+++ module-1/w2/d1/lecture/arrow-functions.js
@@ -0,0 +1,22 @@
+
+
+
+
+let myFunc = (param1) => ({name:'brandon'});
+
+console.log(myFunc(12));
+
+
+// let obj = {
+//   name: 'brandon'
+// }
+
+// if(){}
+
+// for(){}
+let obj = {
+  key: 'value',
+  addTwo: function(num){
+    return num + 2
+  }
+}
