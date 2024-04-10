8011ce23d8cf6d94d2a225b48da4216eea46b2c1
brandon <brandonlaursen95@yahoo.com>
2024-04-10 11:18:22 -0400

    forEach

---
+++ module-1/w2/d3/lecture/arr-methods.js
@@ -0,0 +1,12 @@
+const array1 = ["a", "b", "c"];
+
+array1.forEach((element, i, arr) => console.log(element, i, arr));
+
+
+let numbers  = [10, 20, 30, 40, 50]
+
+let sum = 0;
+
+numbers.forEach( (num) => sum += num  );
+
+console.log(sum);// 150
