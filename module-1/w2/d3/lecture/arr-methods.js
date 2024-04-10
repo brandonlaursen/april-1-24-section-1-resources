88f5010a2ee2c6ae9f8eeda3b6b88769229fbf8e
brandon <brandonlaursen95@yahoo.com>
2024-04-10 11:31:56 -0400

    map

--- module-1/w2/d3/lecture/arr-methods.js
+++ module-1/w2/d3/lecture/arr-methods.js
@@ -1,12 +1,43 @@
 const array1 = ["a", "b", "c"];
 
-array1.forEach((element, i, arr) => console.log(element, i, arr));
+// array1.forEach((element, i, arr) => console.log(element, i, arr));
 
-
-let numbers  = [10, 20, 30, 40, 50]
+let numbers = [10, 20, 30, 40, 50];
 
 let sum = 0;
 
-numbers.forEach( (num) => sum += num  );
+numbers.forEach((num) => (sum += num));
 
-console.log(sum);// 150
+// console.log(sum);// 150
+
+// .map
+const array2 = [1, 4, 9, 16];
+
+// Pass a function to map
+const map1 = array2.map(function (x) {
+  //  x * 2;
+  if (x % 2 !== 0) {
+    return true;
+  }
+  return false;
+});
+
+// const [2,8,18,32 ] = array1.map( (x) => x * 2 );
+
+// console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
+
+function timesTwo(num) {
+  return num * 2
+}
+// console.log([2,4,6].map(timesTwo));// [ 4, 8, 12 ]
+
+
+let strings = ['a', 'an', 'array', 'of', 'string'];
+
+
+let upperCased = strings.map((string) => string.toUpperCase())
+//.join(' ')
+
+// console.log(upperCased);
+// [ 'A', 'AN', 'ARRAY', 'OF', 'STRING' ]
