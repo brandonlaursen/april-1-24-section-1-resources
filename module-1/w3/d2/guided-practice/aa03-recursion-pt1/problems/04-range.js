39f7a85ba09e1662149b9542771caa58706fd7f2
brandon <brandonlaursen95@yahoo.com>
2024-04-16 13:53:46 -0400

    range

--- module-1/w3/d2/guided-practice/aa03-recursion-pt1/problems/04-range.js
+++ module-1/w3/d2/guided-practice/aa03-recursion-pt1/problems/04-range.js
@@ -10,12 +10,37 @@ range(3, 4); // [3]
 range(7, 6); // []
 ***********************************************************************/
 
+// base case: end <= start || start >= end
+// recursive case: start + 1  || end - 1
 
-function range(start, end) {
-  // Your code here 
+function range(start, end, arr = []) {
+
+  if (start >= end) return arr;
+  // if(end <= start) return [];
+
+  arr.push(start)
+  return range(start + 1, end, arr)
+  // return [...range(start, --end), end];
+  // let recurse = range(start, --end);
+  // recurse.push(end);
+  // return recurse;
+  // return [start].concat(range(start + 1, end));
 }
 
+/*
+i++
+i += 1
+i = i + 1
 
+range(1, 1)  => []
+range(1, 2)  => [1]
+range(1, 3)  => [1, 2]
+
+range(1, 4)  => [1, 2, 3]
+range(1, 5)  => [1, 2, 3, 4]
+*/
+
+console.log(range(1, 5)); // [1, 2, 3, 4])
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = range;
