ae777a28f27f03a07f9f224e6cb28417e7ae6202
brandon <brandonlaursen95@yahoo.com>
2024-04-22 19:21:27 -0400

    srp

--- module-2/w4/d1/guided-practice/02-single-responsibility-principle-temp/practice.js
+++ module-2/w4/d1/guided-practice/02-single-responsibility-principle-temp/practice.js
@@ -1,73 +1,94 @@
-/******************************** CONSTANTS *********************************/ 
+/******************************** CONSTANTS *********************************/
 const applePieRecipe = [
-  { name: "pie crust", cost: 10.00, quantity: 1 },
-  { name: "sugar", cost: 3.00, quantity: 0.5 },
-  { name: "butter", cost: 1.00, quantity: 1 },
-  { name: "apples", cost: 6.00, quantity: 7 },
-  { name: "cinnamon", cost: 5.50, quantity: 1 },
-  { name: "eggs", cost: 2.00, quantity: 1 },
+  { name: "pie crust", cost: 10.0, quantity: 1 },
+  { name: "sugar", cost: 3.0, quantity: 0.5 },
+  { name: "butter", cost: 1.0, quantity: 1 },
+  { name: "apples", cost: 6.0, quantity: 7 },
+  { name: "cinnamon", cost: 5.5, quantity: 1 },
+  { name: "eggs", cost: 2.0, quantity: 1 },
 ];
 
 const pumpkinPieRecipe = [
-  { name: "pie crust", cost: 10.00, quantity: 1 },
-  { name: "sugar", cost: 3.00, quantity: 0.5 },
-  { name: "butter", cost: 1.00, quantity: 1 },
+  { name: "pie crust", cost: 10.0, quantity: 1 },
+  { name: "sugar", cost: 3.0, quantity: 0.5 },
+  { name: "butter", cost: 1.0, quantity: 1 },
   { name: "pumpkin", cost: 3.75, quantity: 2 },
-  { name: "cinnamon", cost: 5.50, quantity: 1 },
-  { name: "eggs", cost: 2.00, quantity: 1 },
+  { name: "cinnamon", cost: 5.5, quantity: 1 },
+  { name: "eggs", cost: 2.0, quantity: 1 },
 ];
 
 const cherryPieRecipe = [
-  { name: "pie crust", cost: 10.00, quantity: 1 },
-  { name: "sugar", cost: 3.00, quantity: 0.5 },
-  { name: "butter", cost: 1.00, quantity: 1 },
-  { name: "cherries", cost: 12.00, quantity: 10 },
-  { name: "eggs", cost: 2.00, quantity: 1 },
+  { name: "pie crust", cost: 10.0, quantity: 1 },
+  { name: "sugar", cost: 3.0, quantity: 0.5 },
+  { name: "butter", cost: 1.0, quantity: 1 },
+  { name: "cherries", cost: 12.0, quantity: 10 },
+  { name: "eggs", cost: 2.0, quantity: 1 },
 ];
 
 const recipes = {
   applePie: applePieRecipe,
   pumpkinPie: pumpkinPieRecipe,
-  cherryPie: cherryPieRecipe
+  cherryPie: cherryPieRecipe,
 };
 /* DO NOT CHANGE THE CODE ABOVE */
 
-/*************************** FUNCTION TO REFACTOR ****************************/ 
-function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
-  // Find the recipe for the pieType specified
+// Baking pies
+function bakingPies(pieType, pieQuantity) {
   const recipe = recipes[pieType];
   // Bake the number of pies specified by the pieQuantity
   for (let i = 0; i < pieQuantity; i++) {
     // Print the ingredients for each ingredient in the recipe
-    let combiningMsg = `Combining ingredients for ${pieType}: `
-    combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
+    let combiningMsg = `Combining ingredients for ${pieType}: `;
+    combiningMsg += recipe.map((ingredient) => ingredient.name).join(", ");
     console.log(combiningMsg);
 
     // Print the nth pie that was baked
     console.log(`Baked pie ${i + 1}!`);
   }
+}
 
+// console.log(bakingPies("applePie", 5));
+
+// print pie cost
+function pieCost(pieType) {
+  const recipe = recipes[pieType];
   // Print the cost of each pie based on the cost of each ingredient
   const costOfPie = recipe.reduce((prev, current) => {
     return prev + current.cost;
   }, recipe[0].cost);
   console.log(`Cost per pie: ${costOfPie}`);
+  return costOfPie;
+}
 
+// console.log(pieCost("applePie"));
+
+// sell pie
+function sellPie(costOfPie, pieQuantity, profitMargin) {
   // Calculate the total cost of all the pies
   const totalCost = costOfPie * pieQuantity;
 
   // Print the total revenue calculated using the given profitMargin
   const revenue = totalCost * (profitMargin || 1.2);
   console.log(`Sold ${pieQuantity} pies for $${revenue.toFixed(2)}!`);
+
+}
+
+/*************************** FUNCTION TO REFACTOR ****************************/
+function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
+  // Find the recipe for the pieType specified
+
+  bakingPies(pieType, pieQuantity);
+  let costOfPie = pieCost(pieType);
+  sellPie(costOfPie, pieQuantity, profitMargin)
 }
 
 /******************************* LOCAL TESTS *******************************/
-// bakeAndSellPies("applePie", 5, 2.5);
+bakeAndSellPies("applePie", 5, 2.5);
 // bakeAndSellPies("pumpkinPie", 2);
 // bakeAndSellPies("cherryPie", 7, 1.7);
 
 /**************************************************************************/
 /* DO NOT CHANGE THE CODE BELOW */
 module.exports = {
-  bakeAndSellPies
+  bakeAndSellPies,
 };
