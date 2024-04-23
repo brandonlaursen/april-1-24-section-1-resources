8fd74f52a869c694b1c7caa21c41e962d27c1577
brandon <brandonlaursen95@yahoo.com>
2024-04-23 14:12:17 -0400

    class syntax

--- module-2/w4/d2/guided-practice/02-class-basic-syntax/classes/person.js
+++ module-2/w4/d2/guided-practice/02-class-basic-syntax/classes/person.js
@@ -1,4 +1,47 @@
-// Your code here 
+
+
+
+
+
+
+class Person {
+
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    // this.introduce();
+  };
+
+  introduce(){
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  };
+
+  static introducePeople(people){
+    if(!Array.isArray(people)){
+      throw new Error( "introducePeople only takes an array as an argument.")
+    }
+
+    for(let person of people) {
+      if(!(person instanceof Person)) {
+        throw new Error( "All items in array must be Person class instances.")
+      }
+    }
+
+    people.forEach(person => person.introduce());
+  }
+
+}
+
+
+
+
+let person = new Person("kristen", "chauncey", 38);
+// console.log(person);
+const person2 = new Person("jeff", "granoff", 30);
+
+Person.introducePeople([person, person2]);
+// Person.introducePeople(person);
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
