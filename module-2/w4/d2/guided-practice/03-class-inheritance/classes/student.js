bbc873f3ee5347fa35e365d55eac15f0e0a10d17
brandon <brandonlaursen95@yahoo.com>
2024-04-23 17:04:28 -0400

    o3 and 04

--- module-2/w4/d2/guided-practice/03-class-inheritance/classes/student.js
+++ module-2/w4/d2/guided-practice/03-class-inheritance/classes/student.js
@@ -1,6 +1,39 @@
+
+
+
+
+
+
+
+
 const Person = require('./person');
 
-// Your code here 
+class Student extends Person{
+  constructor(firstName,lastName, major, GPA ){
+    super(firstName,lastName)
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(student1, student2){
+
+
+    if(student1.GPA > student2.GPA) {
+      return `${student1.firstName} ${student1.lastName} has the higher GPA.`
+    } else if(student1.GPA < student2.GPA) {
+      return `${student2.firstName} ${student2.lastName} has the higher GPA.`
+    } else {
+      return  "Both students have the same GPA"
+    }
+  }
+}
+
+
+// const student1 = new Student("billy", "johnson", "software-development", 4);
+
+// const student2 = new Student("sarah", "smith", "basket-weaving", 4);
+
+// console.log(Student.compareGPA(student1, student2))
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- module-2/w4/d2/guided-practice/03-class-inheritance/classes/teacher.js
+++ module-2/w4/d2/guided-practice/03-class-inheritance/classes/teacher.js
@@ -1,7 +1,31 @@
 const Person = require('./person');
 
-// Your code here 
 
+class Teacher extends Person{
+  constructor(firstName, lastName, subject, yearsOfExperience){
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(...teachers){
+    console.log(teachers)
+    // let count = 0;
+    // for(const teacher of teachers){
+    //   count += teacher.yearsOfExperience;
+    // };
+
+    // return count;
+    return teachers.reduce((count, teacher) => count += teacher.yearsOfExperience,0)
+  }
+}
+
+
+
+// teacher1 = new Teacher("susan", "jones", "biology", 5);
+// teacher2 = new Teacher("bobby", "roberts", "math", 15);
+
+// console.log(Teacher.combinedYearsOfExperience([teacher1, teacher2]))
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
