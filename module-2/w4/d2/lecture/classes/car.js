c4e75e4a4206f9d114521bbe86111f387eeb0c0d
brandon <brandonlaursen95@yahoo.com>
2024-04-23 16:05:00 -0400

    lecture classes notes

--- module-2/w4/d2/lecture/classes/car.js
+++ module-2/w4/d2/lecture/classes/car.js
@@ -1,48 +1,42 @@
 
-const obj = { name: 'challenger', make: 'dodge', model: 'hellcat', makeNoise: () =>  `${this.name} Car goes vrooom`  }
+
 
 
 class Car {
-  constructor(name, make, model){
+  constructor(name, make, model) {
     this.name = name;
     this.make = make;
     this.model = model;
-    Car.numCars += 1;
-  };
-
-
-  makeNoise(){
-    return `${this.name} Car goes vrooom`;
-  };
-
-  static numCars = 0;
-
-  static getCars(...cars) {
-    cars.forEach(car => {
-      if(!car instanceof Car) {
-        console.log(car.makeNoise());
-        // throw new Error('that aint a car');
-      }
-    });
-
-    // return cars.map(car => car.name);
+    this.noise = 'vrooom';
+    Car.numVehicles += 1;
   }
 
-  // instanceoF
+  makeNoise() {
+    return `${this.name} Car goes ${this.noise}`;
+  }
 
+  static numVehicles = 0;
+
+  static getCars(...cars) {
+    return cars.map((car) => car.name);
+  }
 }
 
-const challenger = new Car('challenger', 'dodge', 'hellcat');
+const challenger = new Car("challenger", "dodge", "hellcat");
 // console.log(challenger);// Car { name: 'challenger', make: 'dodge', model: 'hellcat' }
-const mustang = new Car('mustang', 'ford', 'shelby');
+const mustang = new Car("mustang", "ford", "shelby");
 // console.log(mustang);// Car { name: 'mustang', make: 'ford', model: 'shelby' }
 // console.log(mustang.makeNoise());
 // console.log(challenger.makeNoise());
 // console.log(Car.numCars);
 // console.log(mustang.numCars)
-console.log(Car.getCars(challenger, mustang));
+// console.log(Car.getCars(challenger, mustang));
 // console.log(Car.makeNoise());
 
 // let arr = [1,2,3];
 // let arr1 = new Array(1,2,3)
 // arr1.slice()
+
+// const obj = { name: 'challenger', make: 'dodge', model: 'hellcat', makeNoise: () =>  `${this.name} Car goes vrooom`  }
+
+module.exports = Car;
---
+++ module-2/w4/d2/lecture/classes/motorcycle.js
@@ -0,0 +1,52 @@
+
+
+const Car = require('./car');
+
+
+
+class Motorcycle extends Car{
+  constructor(name, make, model, milage){
+    super(name, make, model);
+
+    this.milage = milage;
+    this.noise = 'humbrubrubuhrburbhbh';
+    Motorcycle.numVehicles += 1
+
+  }
+
+  makeNoise() {
+    return `${this.name} Motorcycle goes humbrubrubuhrburbhbh`;
+  };
+
+  static numVehicles = 0;
+
+}
+
+const harley = new Motorcycle('Harley', 'harley davidson', 'sportster', 50);
+
+
+class Bicycle extends Motorcycle{
+
+}
+
+const bike = new Bicycle('speed','sschwin', 'beach crusier', '?');
+console.log(bike)
+
+// console.log(harley.makeNoise());
+// console.log(Motorcycle.numVehicles)
+
+class Truck extends Car{
+};
+
+const F150 = new Truck('f150', 'ford', 'raptor');
+// console.log(F150);
+
+function myFunc(){
+  let name = 'brandon';
+
+  function myfunc2(){
+    // let name = 'maica';
+
+    console.log(name)
+  }
+}
