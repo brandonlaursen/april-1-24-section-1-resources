de5a4cb9fa75ddc7fd22009863fecc422c26d475
brandon <brandonlaursen95@yahoo.com>
2024-04-24 12:39:34 -0400

    01 + 02

--- module-2/w4/d3/guided-practice/02-bind/employee.js
+++ module-2/w4/d3/guided-practice/02-bind/employee.js
@@ -1,1 +1,24 @@
-// Your code here 
+// Your code here
+
+class Employee {
+  constructor(name, occupation) {
+    this.name = name;
+    this.occupation = occupation;
+  }
+
+  sayName() {
+    console.log(`${this.name} says hello`);
+  }
+  sayOccupation() {
+    console.log(`${this.name} is a ${this.occupation}`);
+  }
+}
+
+// const employee1 = new Employee('brandon', 'instructor')
+// // console.log(employee1);
+
+// try {
+//   module.exports = Employee;
+// } catch {
+//   module.exports = null;
+// }
--- module-2/w4/d3/guided-practice/02-bind/test.js
+++ module-2/w4/d3/guided-practice/02-bind/test.js
@@ -1,1 +1,15 @@
-// Your code here 
+// Your code here
+
+const Employee = require('./employee');
+
+const john = new Employee('John Wick', 'Dog Lover');
+
+setTimeout(() => john.sayName(), 2000);
+
+// context is setTimeout object
+// not bound!
+
+let sayNameBound = john.sayName.bind(john)
+
+setTimeout(sayNameBound, 2000);
+//
