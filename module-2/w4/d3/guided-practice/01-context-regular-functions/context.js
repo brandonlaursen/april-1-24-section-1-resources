c7dabb9f511aa8b72db3026e4a55c3fe35ffb67c
brandon <brandonlaursen95@yahoo.com>
2024-04-24 12:22:52 -0400

    context

--- module-2/w4/d3/guided-practice/01-context-regular-functions/context.js
+++ module-2/w4/d3/guided-practice/01-context-regular-functions/context.js
@@ -15,21 +15,22 @@ class Fish {
 const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+// eat(); //  accessing global object, it does not have a key name
 
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
-// nemo.eat(); // ?
+// nemo.eat(); // added the eat function as key/value pair
+// the nemo object is what is calling the method
 
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
-// eat(); // ?
+// eat(); // doing same thing as line 18
 
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+nemo.swim(); // the nemo object is calling the swim method
 
 
 /********************************* Scenario 5 *********************************/
