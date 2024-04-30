2c9dba94d166f7a7609bdf84f8290c4ebe580d0e
brandon <brandonlaursen95@yahoo.com>
2024-04-30 16:33:09 -0400

    dynamic arrays

--- module-2/w5/d2/guided-practice/03-aa05-dynamic-array-basics/dynamic-array.js
+++ module-2/w5/d2/guided-practice/03-aa05-dynamic-array-basics/dynamic-array.js
@@ -1,22 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
 class DynamicArray {
 
   constructor(defaultSize=4) {
 
-    // Your code here 
+    // [99, <3 empty items>]
+    this.data = new Array(defaultSize);
+    this.length = 0;
+    this.capacity = defaultSize;
 
   }
 
   read(index) {
 
-    // Your code here 
+
+    if(index >= 0 && index < this.length) return this.data[index]
+
+    return undefined;
+  }
+
+  resize(){
+    this.capacity = this.capacity * 2;
   }
 
   unshift(val) {
 
-    // Your code here 
+    //  0  1  2
+    // [2, 1] undefined
+    // [2, 1, 1]
+    // [2, 2, 1]
+    // [3, 2, 1]
+
+    // if(this.capacity <= this.length) resize();
+
+
+    for(let i = this.length; i > 0; i--) {
+
+      this.data[i] = this.data[i - 1];
+      console.log(this.data);
+    }
+
+    this.data[0] = val;
+    this.length++;
+    console.log(this.data);
+    return this.length;
   }
 
 }
 
+const dynamicArr = new DynamicArray(4);
+dynamicArr.unshift(1);
+dynamicArr.unshift(2);
+console.log(dynamicArr.unshift(3));
+dynamicArr.unshift(4);
+dynamicArr.unshift(5);
+
+// console.log(dynamicArr.read(1));
 
 module.exports = DynamicArray;
