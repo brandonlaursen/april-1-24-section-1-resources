23a759094371b44680433159523977afc300653b
brandon <brandonlaursen95@yahoo.com>
2024-05-02 16:12:57 -0400

    insert

--- module-2/w5/d4/guided-practice/01-hash-tables-basics/hash-table.js
+++ module-2/w5/d4/guided-practice/01-hash-tables-basics/hash-table.js
@@ -11,27 +11,58 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here 
+    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
-    // Your code here 
+    // Your code here
+    // grab first 8 characters of sha256 hash
+    const hashString = sha256(key).slice(0, 8);
+
+    // turning into number
+    return parseInt(`${hashString}`, 16);
   }
 
   hashMod(key) {
-    // Your code here 
+    // Your code here
+
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here 
+    // Your code here
+
+    // if(this.count / this.capacity > .7) {
+    //   // resizeBy(
+
+    //   this.capacity = this.capacity * 2;
+    // }
+    const index = this.hashMod(key);
+
+    if(!this.data[index]) {
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      throw Error('hash collision or same key/value pair already exists!');
+    }
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here 
+    // Your code here
   }
 
   insert(key, value) {
-    // Your code here 
+    // Your code here
+
+    // run key through hash mod function and get index
+    // see if their is a current pair at the index
+    // iterate to check if the current pair exists and is not the key
+    // if current pair exists, overwrite the value
+    // if its doesnt create the key/value pair and if the position is empty, set the pair at the position
+    // if we traverse and dont find the key, then add the key-value pair to the head and point at old head
   }
 
 }
