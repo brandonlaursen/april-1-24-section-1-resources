c27a8a4f3bd822bf24669b55ff49213f1a06c944
brandon <brandonlaursen95@yahoo.com>
2024-05-06 16:04:01 -0400

    swapping

--- module-2/w6/d1/lecture/lecture.js
+++ module-2/w6/d1/lecture/lecture.js
@@ -2,11 +2,11 @@
 
 
 
-let arr = [0, 1, 2, 3];
+let arr = [0, 1, 0, 3];
 
 // * Swap two values with a temporary variable
-for(let i = 0; i < arr.length; i++){
+for(let i = 0; i < arr.length - 1; i++){
 
   [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-
+  // console.log(arr);
 }
