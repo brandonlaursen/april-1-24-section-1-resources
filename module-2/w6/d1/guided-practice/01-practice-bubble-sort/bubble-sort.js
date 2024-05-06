3ec28a5dce0817d825948316def6b1013e93f00d
brandon <brandonlaursen95@yahoo.com>
2024-05-06 18:33:21 -0400

    insertion sort

--- module-2/w6/d1/guided-practice/01-practice-bubble-sort/bubble-sort.js
+++ module-2/w6/d1/guided-practice/01-practice-bubble-sort/bubble-sort.js
@@ -1,5 +1,3 @@
-
-
 /*
 
 
@@ -24,19 +22,53 @@
 
 */
 function bubbleSort(arr) {
+  let hasSwapped = true;
+  // Iterate through the array
+  while (hasSwapped) {
+    hasSwapped = false;
 
-    // Iterate through the array
-
+    for (let i = 0; i < arr.length; i++) {
       // If the current value is greater than its neighbor to the right
+      if (arr[i] > arr[i + 1]) {
         // Swap those values
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        hasSwapped = true;
 
         // Do not move this console.log
         console.log(arr.join(","));
+      }
+    }
 
     // If you get to the end of the array and no swaps have occurred, return
 
     // Otherwise, repeat from the beginning
+  };
+  return arr;
+};
 
-}
+// function bubbleSort(arr) {
+//   for(let i = 0; i < arr.length - 1; i++){
+//     for(let j = 0; j < arr.length - 1 - i; j++){
+//       if(arr[j] > arr[j + 1]){
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+//         console.log(arr.join(","));
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// for (let i = 0; i < arr.length - 1; i++) {
+//       for (let j = 0; j < arr.length - 1; j++)
+//         // Iterate through the array
+//         if (arr[j] > arr[j + 1]) {
+//           let temp = arr[j];
+//           arr[j] = arr[j + 1];
+//           arr[j + 1] = temp;
+//           console.log(arr.join(","));
+//         }
+//       }
+
+console.log("==>", bubbleSort([2, 4, 0, 8, 1])); // [ 0, 1, 2, 4, 8 ]
 
 module.exports = bubbleSort;
--- module-2/w6/d1/guided-practice/02-insertion-sort-temp/insertion-sort.js
+++ module-2/w6/d1/guided-practice/02-insertion-sort-temp/insertion-sort.js
@@ -23,18 +23,15 @@
 
 
   Insertion Sort sorts an array by sequentially inserting each element into its proper position within the initially empty sorted portion. It compares each element with those in the sorted section, shifting elements one position right to make space, thereby gradually extending the sorted array until completion.
-  
+
 
 */
 
 
 // Insertion Sort out-of-place
 // Do not modify the original array
-function insertionSort(arr) {
-  /*
-  Pseudocode:
-
-  Copy the original array
+/*
+Copy the original array
   Create an array to store the sorted values
   While the array is not empty:
   - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
@@ -47,30 +44,66 @@ function insertionSort(arr) {
   - Insert the unsorted value at the break point
   Return the sorted array
   */
+function insertionSort(arr) {
 
-  // Your code here
-}
+  let copyArr = [...arr];
+
+  const sorted = [];
+
+  while(copyArr.length > 0) {
+    console.log(sorted.join(','))
+    let val = copyArr.pop();
+
+    sorted.push(null);
+
+    let i = sorted.length - 1
+    while(i > 0 ){
+      if(sorted[i - 1] < val) {
+        break;
+      } else {
+        sorted[i] = sorted[i - 1];
+        i--;
+      }
+    };
+
+    sorted[i] = val;
+  }
+
+  return sorted;
+
+};
+
+// console.log("==>", insertionSort([2, 4, 0, 8, 1])); // [ 0, 1, 2, 4, 8 ]
 
 // In-place Insertion Sort
 // Mutates the original array
 function insertionSortInPlace(arr) {
-  /*
-  Pseudocode:
 
-  Set a pointer dividing the array into sorted and unsorted halves
-  Repeat while the unsorted half is not empty:
-  - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
-  - Grab the first value from the unsorted half
-  - For each value starting from the divider,
-  - Check if the value to the left is smaller than the unsorted value
-  - If so, you've reached the insertion point so exit the loop
-  - If not shift the value to the right by 1 and continue
-  - Insert the unsorted value at the break point
-  - Increment the dividing pointer and repeat
-  Return the mutated array
-  */
+  let divider = 1;
 
-  // Your code here
+  while(divider < arr.length) {
+    let val = arr[divider];
+
+    let i = divider;
+
+    while(i > 0) {
+      if(arr[i - 1] < val) {
+        break;
+      } else {
+        arr[i] = arr[i - 1];
+        i--;
+      }
+    }
+
+    arr[i] = val;
+    divider++;
+  }
+
+  return arr;
+
 }
+
+
+console.log("==>", insertionSortInPlace([2, 4, 0, 8, 1])); // [ 0, 1, 2, 4, 8 ]
 
 module.exports = [insertionSort, insertionSortInPlace];
