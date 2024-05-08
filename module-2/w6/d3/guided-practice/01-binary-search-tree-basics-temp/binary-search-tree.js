11c047c519de0f6882af3670823c48106cfbc142
brandon <brandonlaursen95@yahoo.com>
2024-05-08 12:07:48 -0400

    insert

--- module-2/w6/d3/guided-practice/01-binary-search-tree-basics-temp/binary-search-tree.js
+++ module-2/w6/d3/guided-practice/01-binary-search-tree-basics-temp/binary-search-tree.js
@@ -8,43 +8,70 @@ class TreeNode {
 }
 
 class BinarySearchTree {
-
   constructor() {
-    // Your code here 
+    // Your code here
+    this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
-    // Your code here 
+  insert(val, currentNode = this.root) {
+    // Your code here
+    const newNode = new TreeNode(val);
+
+    if (!this.root) {
+      this.root = newNode
+      return;
+    };
+
+    if(val < currentNode.val) {
+
+      if(!currentNode.left) {
+        currentNode.left = newNode
+      } else {
+        this.insert(val, currentNode.left);
+      }
+
+    } else {
+
+      if(!currentNode.right) {
+        currentNode.right = newNode
+      } else {
+        this.insert(val, currentNode.right);
+      }
+    }
   }
 
   search(val) {
-    // Your code here 
+    // Your code here
   }
-
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
-
 
   inOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
-
 
   postOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
 
-    // Breadth First Traversal - Iterative
+  // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
-    // Your code here 
+    // Your code here
   }
 
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
-    // Your code here 
+    // Your code here
   }
 }
+
+let bst = new BinarySearchTree();
+bst.insert(4);
+bst.insert(2);
+bst.insert(6);
+bst.insert(1);
+// console.log(bst.root.left);
 
 module.exports = { BinarySearchTree, TreeNode };
