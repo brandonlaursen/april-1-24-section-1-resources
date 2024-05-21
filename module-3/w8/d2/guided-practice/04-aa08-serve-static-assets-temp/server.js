40652ff52847c6bf830c65d993b86fa2d26e456e
Maica <maicasantos90@gmail.com>
2024-05-21 15:08:10 -0700

    4 out of 6

--- module-3/w8/d2/guided-practice/04-aa08-serve-static-assets-temp/server.js
+++ module-3/w8/d2/guided-practice/04-aa08-serve-static-assets-temp/server.js
@@ -1,8 +1,72 @@
 const http = require('http');
 const fs = require("fs");
 
+function getContentType(fileName){
+  // need the ext
+  const ext = fileName.split(".")[1]
+
+  // console.log("EXT ->",ext);
+
+  let contentType = 'text/plain';
+  switch(ext) {
+    case "jpeg":
+    case "jpg":
+      contentType = "image/jpeg"
+      break;
+    case "png":
+      contentType = "image/png"
+      break;
+    case "css":
+      contentType = "text/css"
+      break;
+    default: contentType = "text/plain"
+      break;
+  }
+
+  return contentType
+}
+
 const server = http.createServer((req, res) => {
-  // Your code here 
+  // Your code here
+  // res.end("Hello! ")
+
+  // Phase 2
+  if (req.method === "GET" && req.url.startsWith("/static")){
+    //split to gain access to the assetPath
+    const assetPath = req.url.split("/static")[1] // '/css/application.css'
+    // console.log(assetPath);
+
+    // try/catch in case we run into an error
+    try {
+      const resBody = fs.readFileSync("./assets" + assetPath);
+
+      res.statusCode = 200;
+
+      // set header
+      // create a helper func to fund out the content
+      res.setHeader("Content-Type", getContentType(assetPath))
+
+      res.end(resBody);
+      return;
+
+
+    } catch {
+      console.error("Cannot find asset", assetPath, "in assets folder");
+      res.statusCode = 404;
+      return res.end();
+    }
+
+  }
+
+
+  // Phase 1
+  const resBody = fs.readFileSync("./index.html")
+
+  res.statusCode = 200;
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.end(resBody);
 });
 
 const port = 5000;
