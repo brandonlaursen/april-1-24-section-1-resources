1e125f122206fcec24ddd3fedd1b1a4163d89c1d
brandon <brandonlaursen95@yahoo.com>
2024-05-21 16:45:22 -0400

    route handlers

--- module-3/w8/d2/guided-practice/03-aa08-route-handlers-temp/server.js
+++ module-3/w8/d2/guided-practice/03-aa08-route-handlers-temp/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 
 let nextDogId = 1;
 
@@ -35,14 +35,92 @@ const server = http.createServer((req, res) => {
 
     // define route handlers here
 
+    // * GET /
+    if (req.method == "GET" && req.url === "/") {
+      // response
+      // status line
+      res.statusCode = 200;
+      // headers
+      res.setHeader("Content-Type", "text/plain");
+      // body
+      const resBody = "Dog Club";
+      // end the response;
+      return res.end(resBody);
+    }
+
+    // * GET /dogs
+    if (req.method === "GET" && req.url === "/dogs") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      const resBody = "Dogs index";
+
+      return res.end(resBody);
+    }
+
+    // * GET /dogs/new
+    if (req.method === "GET" && req.url === "/dogs/new") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end("Dog create form page");
+    }
+
+
+
+
+    // * GET /dogs/:dogId/edit
+    if(req.method === "GET" && req.url.endsWith('/edit')) {
+      const url = req.url.split("/");// [ '', 'dogs', '1', 'edit' ]
+
+      const dogId = url[2];
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(`Dog edit form page for dogId: ${dogId}`)
+
+    }
+
+
+    // * GET /dogs/:dogId
+    if (req.method === "GET" && req.url.startsWith("/dogs")) {
+      // console.log(req.url)// /dogs/1
+      const url = req.url.split("/");
+      // console.log(url);// [ '', 'dogs', '1' ]
+
+      const dogId = url[2];
+      const resBody = `Dog details for dogId: ${dogId}`;
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(resBody);
+    }
+
+    // * POST /dogs
+    if(req.method === 'POST' && req.url === '/dogs') {
+      res.statusCode = 302;
+      res.setHeader('Location', '/dogs/' + getNewDogId());
+
+      return res.end();
+    }
+
+
+    // * POST /dogs/:dogId
+    if (req.method === "POST" && req.url.startsWith("/dogs")) {
+      res.statusCode = 302;
+      const url = req.url.split("/");
+      // console.log(url);// [ '', 'dogs', '1' ]
+
+      const dogId = url[2];
+
+      res.setHeader('Location', '/dogs/' + dogId);
+      return res.end();
+    };
+
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('No matching route handler found for this endpoint');
+    res.setHeader("Content-Type", "text/plain");
+    return res.end("No matching route handler found for this endpoint");
   });
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
