e4fc682960850cee0750449bc4c12dec97dfb91e
brandon <brandonlaursen95@yahoo.com>
2024-05-30 13:59:19 -0400

    end of lecture

--- module-3/w9/d4/guided-practice/02-aa09-data-on-elements-temp/data.js
+++ module-3/w9/d4/guided-practice/02-aa09-data-on-elements-temp/data.js
@@ -1,1 +1,29 @@
-// Your code here 
+// Your code here
+
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  const addButton = document.getElementById("add");
+
+  addButton.addEventListener("click", (e) => {
+
+    // e.preventDefault();
+
+    const shoppingList = document.getElementById("shopping-list");
+
+    const name = document.getElementById("name");
+
+    const type = document.getElementById("type");
+
+    const li = document.createElement("li");
+
+    li.innerText = name.value;
+
+    li.dataset.type = type.value;
+
+    shoppingList.appendChild(li);
+  });
+});
